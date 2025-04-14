18cf14f67d0344ba41b97cb8149339fb82eacbf8
Fahris Nassief <fahrisnassief@Fahriss-MacBook-Pro.local>
2025-04-13 20:40:23 -0600

    Added initial routing as well as homepage. Also did some rough styling

--- portfolio-app/src/app/app.component.ts
+++ portfolio-app/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkActive, RouterModule, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLinkActive, RouterModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- portfolio-app/src/app/app.routes.ts
+++ portfolio-app/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectComponent } from './project/project.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'contact', component: ContactComponent},
+    { path: 'resume', component: ResumeComponent},
+    { path: 'project', component: ProjectComponent},
+];
